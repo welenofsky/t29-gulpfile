0200cc1d07026ba015ae46535976716c80d79f1d
Justin Welenofsky <welenofsky@gmail.com>
2016-08-30 20:13:47 -0700

    notify all the things

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,30 @@ var gulp        = require('gulp'),
     cleanCSS    = require('gulp-clean-css'),
     jshint      = require('gulp-jshint'),
     livereload  = require('gulp-livereload'),
+    notify      = require('gulp-notify'),
     uglify      = require('gulp-uglify');
 
 
 gulp.task('js', function() {
   return gulp.src('./wp-content/themes/**/skin/js/custom.js')
     .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'))
+    .pipe(notify(function (file) {
+      if (file.jshint.success) {
+        // Don't show something if success 
+        return false;
+      }
+ 
+      var errors = file.jshint.results.map(function (data) {
+        if (data.error) {
+          return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
+        }
+      }).join("\n");
+      return file.relative + " (" + file.jshint.results.length + " errors)\n" + errors;
+    }))
     .pipe(uglify())
+    .on('error', notify.onError(function (error) {
+        return 'JS Minification failed: ' + error.message;
+    }))
     .pipe(rename(function (path) {
         path.dirname = path.dirname;
         path.basename = "custom";
@@ -30,13 +45,20 @@ gulp.task('less', function () {
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
+    .on('error', notify.onError(function (error) {
+        return 'LESS compilation failed: ' + error.message;
+    }))
     .pipe(cleanCSS())
+    .on('error', notify.onError(function (error) {
+        return 'CSS minification failed: ' + error.message;
+    }))
     .pipe(rename(function (path) {
         path.dirname += "/../skin/css";
         path.basename = "custom-theme";
         path.extname = ".min.css";
     }))
     .pipe(gulp.dest('./wp-content/themes/'))
+    .pipe(notify('LESS compiled successfully!'))
     .pipe(livereload());
 });
 
